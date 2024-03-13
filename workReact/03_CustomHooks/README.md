e36dfced3ffa6e361cc49bd062f51c261c0a8872
loeybho <byo_l@naver.com>
2024-03-13 21:29:35 +0900

    Custom Hook 공부 내용 추가

---
+++ workReact/03_CustomHooks/README.md
@@ -0,0 +1,15 @@
+# Custom Hooks
+
+## Custom Hooks이란?
+
+원하는 기능을 구현하여 나만의 hook을 만들 수 있는 리액트 기능.
+
+## 특징
+
+1. Custom hooks 역시 리액트의 hook이기 때문에 대문자로 함수의 이름을 짓는다. (useInput, useRead etc)
+2. Custom hooks 내에도 useState와 같은 다른 hook을 자유롭게 이용할 수 있다.
+
+## 기능 구현
+
+1. input에 들어오는 value의 상태관리를 하는 useInput 커스텀 훅을 제작한다.
+2. 버튼 클릭시 value값이 자동 초기화 되는 설정을 추가한다.
---
+++ workReact/03_CustomHooks/src/App.jsx
@@ -0,0 +1,21 @@
+import { useInput } from "./useInput";
+
+function displayMessage(message) {
+  alert(message);
+}
+
+function App() {
+  const [inputValue, handleChange, handleSubmit] = useInput("", displayMessage);
+
+  return (
+    <>
+      <div>
+        <h1>useInput</h1>
+        <input value={inputValue} onChange={handleChange} />
+        <button onClick={handleSubmit}>확인</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
