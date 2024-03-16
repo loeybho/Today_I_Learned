0a32b6bf81bae9762cd24c6af259b75edfd6ca3a
loeybho <byo_l@naver.com>
2024-03-16 19:13:48 +0900

    동기 & 비동기

---
+++ workReact/05_props/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header name="React" />
+    </>
+  );
+}
+
+export default App;
---
+++ workReact/05_props/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header({ name }) {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default Header;
